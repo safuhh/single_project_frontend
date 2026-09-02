a5d25a0ff51f535fa735abb7afd94227aa7aab14
Safvan <pcsajitha87@gmail.com>
2026-09-02 20:46:01 +0530

    removed loading ui

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { GoogleOAuthProvider } from "@react-oauth/google";
-import { Provider, useDispatch, useSelector } from "react-redux";
+import { Provider, useDispatch } from "react-redux";
 import { store } from "@/store/redux/store";
 import { useEffect } from "react";
 import api from "@/services/apis/api";
@@ -8,7 +8,6 @@ import { setCredentials, setLoading, logout } from "@/store/redux/authSlice";
 
 function AuthLoader({ children }: { children: React.ReactNode }) {
   const dispatch = useDispatch();
-  const isLoading = useSelector((state: any) => state.auth.isLoading);
 
   useEffect(() => {
     // Cleanup old redux-persist state if exists
@@ -29,17 +28,6 @@ function AuthLoader({ children }: { children: React.ReactNode }) {
 
     checkSession();
   }, [dispatch]);
-
-  if (isLoading) {
-    return (
-      <div className="min-h-screen flex items-center justify-center bg-white">
-        <div className="flex flex-col items-center gap-4">
-          <div className="w-12 h-12 border-4 border-[#0a4d33] border-t-transparent rounded-full animate-spin"></div>
-          <p className="text-sm font-bold uppercase tracking-widest text-slate-400">Verifying session...</p>
-        </div>
-      </div>
-    );
-  }
 
   return <>{children}</>;
 }
